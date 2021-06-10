4f5482dab4373011fc7d392f4aa313e821d955dd
hvinoy <vinoyharsha@gmail.com>
2021-06-09 22:48:09 -0400

    index created

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -5,12 +5,25 @@ import scrape_mars
 
 
 app = Flask(__name__)
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+    # Find one record of data from the mongo database
+    mars_dict = mongo.db.mars_dict.find_one()
+    return render_template("index.html", mars_dict=mars_dict)
+
 
 
 @app.route("/scrape")
 def scraper():
- 
-    marspage = scrape_mars.scrape()
+
+    mars_dict = mongo.db.mars_dict
+    mars_data = scrape_mars.scrape()
+    mars_dict.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
     
 
 if __name__ == "__main__":
